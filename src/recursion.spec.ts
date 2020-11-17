7527b0336032a6bf9b21ebef8515de76491ac6a6
beary <bearyme@outlook.com>
2020-11-17 11:29:22 +0800

    非递归 fibonacci

--- src/recursion.spec.ts
+++ src/recursion.spec.ts
@@ -1,6 +1,11 @@
-import { fibonacci } from './recursion'
+import { fibonacci, fibonacciWithCache } from './recursion'
 
 test('递归获取斐波拉契数列', () => {
   const result = fibonacci(10)
+  expect(result).toEqual(34)
+})
+
+test('缓存获取斐波拉契数列', () => {
+  const result = fibonacciWithCache(10)
   expect(result).toEqual(34)
 })
--- src/recursion.ts
+++ src/recursion.ts
@@ -7,3 +7,19 @@ export const fibonacci = (n: number): number => {
   if (n === 2) return 1
   return fibonacci(n - 2) + fibonacci(n - 1)
 }
+
+/**
+ * 通过缓存前两位值的方式获取斐波拉契数列的第 n 位值
+ * @param n 斐波拉契数列的位数，必须大于 0
+ */
+export const fibonacciWithCache = (n: number) => {
+  let p1 = -1
+  let p2 = 1
+  let curr = 0
+  for (let i = 0; i < n; i++) {
+    curr = p1 + p2
+    p1 = p2
+    p2 = curr
+  }
+  return curr
+}

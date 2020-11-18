f9f173afc0d3024bb9667f986b67319f18557ac6
beary <bearyme@outlook.com>
2020-11-18 23:13:44 +0800

    栈实现斐波拉契

--- src/recursion.spec.ts
+++ src/recursion.spec.ts
@@ -1,4 +1,4 @@
-import { fibonacci, fibonacciWithCache } from './recursion'
+import { fibonacci, fibonacciWithCache, fibonacciWithStack } from './recursion'
 
 test('递归获取斐波拉契数列', () => {
   const result = fibonacci(10)
@@ -7,5 +7,10 @@ test('递归获取斐波拉契数列', () => {
 
 test('缓存获取斐波拉契数列', () => {
   const result = fibonacciWithCache(10)
+  expect(result).toEqual(34)
+})
+
+test('栈获取斐波拉契数列', () => {
+  const result = fibonacciWithStack(10)
   expect(result).toEqual(34)
 })
--- src/recursion.ts
+++ src/recursion.ts
@@ -23,3 +23,20 @@ export const fibonacciWithCache = (n: number) => {
   }
   return curr
 }
+
+/**
+ * 通过栈的方式获取斐波拉契数列的第 n 位值
+ * @param n 斐波拉契数列的位数，必须大于 0
+ */
+export const fibonacciWithStack = (n: number) => {
+  if (n === 1) return 0
+  if (n === 2) return 1
+  const stack = [0, 1]
+  for (let i = 3; i <= n; i++) {
+    const p2 = stack.pop()
+    const p1 = stack.pop()
+    stack.push(p2!)
+    stack.push(p1! + p2!)
+  }
+  return stack.pop()
+}
